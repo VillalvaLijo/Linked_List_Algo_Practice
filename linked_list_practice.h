a3d284f6dc91276aa00188ac09b2db70a41aac5b
Sam Lijo <samuelvillalvalijo@gmail.com>
2021-12-18 15:14:02 -0600

    Starting to build a linked list stack

---
+++ linked_list_practice.h
@@ -0,0 +1,23 @@
+//linked_list_practice.h
+
+//Lord Alya Vijana
+
+//Developing a linked list structure, A Stack and and some nodes to process this c stack
+
+//Develop this so you can eventually read in JSON and then divide it into a linked list.
+
+
+//#ifndef LINKED_LIST_PRACTICE_H_
+//#define LINKED_LIST_PRACTICE_H_
+
+//Possibly need to define the header file in larger implementations of this program.
+
+typedef struct list_item
+{
+    char * name; //Test writing a string into a character pointer in a linked list so you
+                 //don't have to allocate storage
+    long upc;
+    double price;
+}
+
+ 
